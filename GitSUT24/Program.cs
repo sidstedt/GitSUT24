b780dae93df8dde89f36581f39c69d20cb6b06a3
sidstedt <daniel.sidstedt@gmail.com>
2024-09-17 10:37:17 +0200

    added a greeting and username

--- GitSUT24/Program.cs
+++ GitSUT24/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to this super awesome game!");
+            Console.Write("Choose a nickname: ");
+            string userNick = Console.ReadLine();
+            Console.WriteLine($"Greetings, oh exalted {userNick}");
         }
     }
 }
